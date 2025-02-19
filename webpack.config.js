62132157b88d973f8b8190f953a908b0d2b35692
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-02-18 19:59:57 -0500

    Deploy with hashed main.js filename.

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,18 @@ module.exports = {
   mode: 'development',
   entry: './src/scripts/main.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devServer: {
     static: path.resolve(__dirname, 'dist'),
     port: 8080,
     hot: true,
   },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html', title: 'Caching' }),
+  ],
   module: {
     rules: [
       {
